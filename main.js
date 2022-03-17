1eb5095e15d4b7113bc6ea8fe0820b1dc6303941
Eric Hagen <ericjameshagen@gmail.com>
2022-03-16 18:34:56 -0600

    initial commit, manifest, style, icon, and main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('This extension is working');
+
+
+// const modal = document.createElement('div');
+// modal.setAttribute('class', 'modal');
+// const modalContent = document.createElement('div');
+// modalContent.setAttribute('class', 'modal-content');
+// modalContent.innerText = "You've been scrolling a while there!";
+// document.querySelector('body').appendChild(modal).appendChild(modalContent);
