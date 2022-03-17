4d41b70b40eed6513cf8a524ca9c81f2e232e15d
Eric Hagen <ericjameshagen@gmail.com>
2022-03-17 09:51:31 -0600

    edited match urls

--- background.js
+++ background.js
@@ -6,10 +6,15 @@ chrome.runtime.onInstalled.addListener(() => {
 
 
 //High Level Timer Strategy
-    //if user goes to any of the pages listed on manifest, invoke the start of timer
+//[ x ]if user goes to any of the pages listed on manifest, invoke the start of timer
+// --- extension starts up when user goes to all listed websites except twitter
+    
     //timer should run in the background to prevent the timer from stopping when switching between youtube videos, different twitter links etc...
-        //this may mean we need 2 .js files (a background js file and main)
+        
+    //[ x ] this may mean we need 2 .js files (a background js file and main)
+
     //When specified time is up (30s for demo, 15m for full deployment) invoke QUOTE MODAL
+
     //if quote is entered and continue is selected then restart timer for (duration)
 
 // Timer TODO:
@@ -17,12 +22,12 @@ chrome.runtime.onInstalled.addListener(() => {
     //chrome.runtime API
     //need to link background.js to main.js
 
-    function timer () {
-        console.log("timer has started")
-        setTimeout(modalPopUp, 5000)
-    }
-    
-    function modalPopUp () {
-        console.log('Time is up')
-    }
+function timer () {
+    console.log("timer has started")
+    setTimeout(modalPopUp, 10000)
+}
+
+function modalPopUp () {
+    console.log('Time is up')
+}
 
